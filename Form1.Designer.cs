6019695676668577567cf53541eda9c95527b69e
Liam Burton <liam_b92@live.co.uk>
2016-02-18 15:23:41 +0000

    2ND commit
    bit of a break to first as been forgetting to commit back on track been
    working on design of product lay out how it will read code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,25 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // listView1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Cycle Ent";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.listView1.Location = new System.Drawing.Point(36, 65);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(753, 488);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Sinking Ship", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.BurlyWood;
+            this.label2.Location = new System.Drawing.Point(297, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cycle Ent";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.label1);
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.cycling_clipart;
+            this.ClientSize = new System.Drawing.Size(849, 616);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -57,7 +75,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,10 @@ namespace WindowsFormsApplication1
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
